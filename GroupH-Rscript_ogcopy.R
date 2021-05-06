5e3da6c8630c7cc2e7c9e0ccac309d8e730b6613
Charlotte <cfe224@lehigh.edu>
2021-05-06 17:11:54 -0400

    Updated

--- GroupH-Rscript_ogcopy.R
+++ GroupH-Rscript_ogcopy.R
@@ -16,7 +16,7 @@ Females <- 141
 
 Male_Unmarried <- results1 %>% filter(gender=="Male") %>% filter(ever_married =="No") %>% 
   summarize(stroketotal =sum(stroke)*100/Males)
-  
+
 Female_Unmarried <- results1 %>% filter(gender=="Female") %>% filter(ever_married =="No") %>% 
   summarize(stroketotal =sum(stroke)*100/Females)
 
@@ -31,22 +31,16 @@ Married <- results1 %>% filter(ever_married=="Yes") %>% group_by(gender,age) %>%
 
 library(ggplot2)
 ggplot(Married, aes(x=age , y = stroketotal, color=gender)) + geom_bar(stat="identity", position = "dodge") +
-
-  ggtitle("Total Stroke Distribution for Married") + 
-  xlab("Age") + 
+  
+  ggtitle("Total Fare Distribution") + 
+  xlab("Fare") + 
   ylab("Frequency") +
-  scale_x_continuous(breaks = seq(0, 10000, 500))
-
-
-Unmarried <- results1 %>% filter(ever_married=="No") %>% group_by(gender,age) %>% 
-  summarize(stroketotal = sum(stroke)/196)
+  
+  
+  Unmarried <- results1 %>% filter(ever_married=="No") %>% group_by(gender,age) %>% summarize(stroketotal = sum(stroke))
 
 library(ggplot2)
-ggplot(Unmarried, aes(x=age , y = stroketotal, color=gender)) + geom_bar(stat="identity", position = "dodge") +
-
-  ggtitle("Total Stroke Distribution for Unmarried Data") + 
-  xlab("Age") + scale_x_continuous(breaks = seq(0, 10000, 500)) +
-  ylab("Frequency")
+ggplot(Unmarried, aes(x=age , y = stroketotal, color=gender)) + geom_bar(stat="identity")
 
 #5-year categories for x axis
 #convert numbers to percentages -> even out data so males/females are measured 
